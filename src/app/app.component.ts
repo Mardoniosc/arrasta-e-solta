096c53938e917028b83dd8e2fbdea55f3f6c92d0
Mardonio Costa <mardoniocosta@pjc.mt.gov.br>
2022-10-31 10:13:18 -0400

    Exemplo de arrastar e soltar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  file: {
+    lastModified: number;
+    lastModifiedDate: Object;
+    name: string;
+    size: number;
+    type: string;
+    webkitRelativePath: string;
+  } | undefined;
+
   title = 'arrastar-e-soltar';
+
+  icon = '../assets/down.png';
+
+  onFilesAdded(event: any): void {
+    if (event.target.files && event.target.files[0]) {
+      this.file = event.target.files[0];
+      console.log(this.file);
+      console.log(event.target.files[0]);
+      return;
+    }
+  }
 }
